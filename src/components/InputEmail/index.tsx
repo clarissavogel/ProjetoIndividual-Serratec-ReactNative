ebda11a9baab28640450fccb1b4aa3e79197c311
clarissavogel <clarissa.vogel@gmail.com>
2022-11-16 17:57:12 -0300

    Function apagarEmail do input

--- src/components/InputEmail/index.tsx
+++ src/components/InputEmail/index.tsx
@@ -9,19 +9,27 @@ import { Feather } from '@expo/vector-icons';
 
 export const InputEmail = () =>{
 
+    const [email,setEmail] = useState("");
+
+    function apagarEmail(){
+        setEmail("")
+    }
     return(
         <View style={styles.contentInput}>
             <View style={styles.mail}>
                 <Feather name="mail" size={24} color="gray" style={styles.iconmail} />
                 <TextInput 
                     placeholder='E-mail'
+                    value={email}
+                    onChangeText={setEmail}
                     placeholderTextColor={'gray'}
                     keyboardType={'email-address'}
                     style={styles.input}
                 />
             </View>
                 <TouchableOpacity 
-                activeOpacity={0.7}>
+                activeOpacity={0.7}
+                onPress={apagarEmail}>
                     <AntDesign name="closecircle" size={20} color="#FDD400" />
                 </TouchableOpacity>
         </View>
